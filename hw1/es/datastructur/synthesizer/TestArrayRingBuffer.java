9480558c7349ce8ff32b79bebaf77feb57585b3f
Emiliano Zhu <zhu.em@husky.neu.edu>
2020-02-02 16:51:36 -0800

    finished hw1 problem 1 and 2

--- hw1/es/datastructur/synthesizer/TestArrayRingBuffer.java
+++ hw1/es/datastructur/synthesizer/TestArrayRingBuffer.java
@@ -9,6 +9,26 @@ import static org.junit.Assert.*;
 public class TestArrayRingBuffer {
     @Test
     public void someTest() {
-        //ArrayRingBuffer arb = new ArrayRingBuffer(10);
+        ArrayRingBuffer<Double> arb = new ArrayRingBuffer<>(4);
+        assertTrue(arb.isEmpty());
+        assertFalse(arb.isFull());
+
+        assertEquals(4, arb.capacity());
+        assertEquals(0, arb.fillCount());
+
+        arb.enqueue(9.3);
+        arb.enqueue(15.1);
+        arb.enqueue(31.2);
+        arb.enqueue(1.1);
+
+        assertEquals(4, arb.capacity());
+        assertEquals(4, arb.fillCount());
+        assertFalse(arb.isEmpty());
+        assertTrue(arb.isFull());
+
+        assertEquals(9.3, arb.peek(), 0.01);
+        assertEquals(9.3, arb.dequeue(), 0.01);
+        assertEquals(4, arb.capacity());
+        assertEquals(3, arb.fillCount());
     }
 }
