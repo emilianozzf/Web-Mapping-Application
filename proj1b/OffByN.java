ff3f449742d220f48c0e50aec987324e36c4b00f
Emiliano Zhu <zhu.em@husky.neu.edu>
2020-05-02 02:01:48 -0700

    finished OffByN

--- proj1b/OffByN.java
+++ proj1b/OffByN.java
@@ -1,14 +1,26 @@
+/**
+ * A class for off-by-N comparators.
+ */
 public class OffByN implements CharacterComparator {
     private int n;
 
+    /**
+     * Constructor for OffByN.
+     *
+     * @param N - the given N.
+     */
     public OffByN(int N) {
         n = N;
     }
 
+    /**
+     * Returns true if characters are equal by the rules of the implementing class.
+     *
+     * @param x - the character x.
+     * @param y - the character y.
+     */
+    @Override
     public boolean equalChars(char x, char y) {
-        if ((x - y == n) || (y - x == n)) {
-            return true;
-        }
-        return false;
+        return (x - y == n) || (y - x == n);
     }
 }
